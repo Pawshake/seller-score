f240f93a24a3a6b8c6ec3e6e238a7fbb6b886867
Jonas Wouters <jonas@jw.be>
2018-01-15 22:39:41 +0100

    Add other ways to add calculations

--- src/CalculationsCollection.php
+++ src/CalculationsCollection.php
@@ -12,13 +12,62 @@ class CalculationsCollection implements IteratorAggregate
     private $calculations = [];
 
     /**
+     * @param string|int $id
+     * @param Calculation $calculation
+     *
+     * @return CalculationsCollection
+     */
+    public function addCalculationConfiguration($id, Calculation $calculation)
+    {
+        if (isset($this->calculations[$id]) && is_array($this->calculations[$id])) {
+            $this->calculations[$id] = array_merge($this->calculations[$id], [
+                'calculation' => $calculation,
+            ]);
+        } else {
+            $this->calculations[$id] = [
+                'calculation' => $calculation,
+                'input' => null,
+                'maximum_total' => null,
+            ];
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string|int $id
+     * @param int $input
+     * @param int|null $maximumInput
+     *
+     * @return CalculationsCollection
+     */
+    public function addCalculationInput($id, $input, $maximumInput = null)
+    {
+        if (isset($this->calculations[$id]) && is_array($this->calculations[$id])) {
+            $this->calculations[$id] = array_merge($this->calculations[$id], [
+                'input' => $input,
+                'maximum_total' => $maximumInput,
+            ]);
+        } else {
+            $this->calculations[$id] = [
+                'calculation' => null,
+                'input' => $input,
+                'maximum_total' => $maximumInput,
+            ];
+        }
+
+        return $this;
+    }
+
+    /**
      * @param Calculation $calculation
      * @param int $input
      * @param int $maximumTotal
      *
      * @return CalculationsCollection
      */
-    public function addCalculation(Calculation $calculation, $input, $maximumTotal = null) {
+    public function addCalculation(Calculation $calculation, $input, $maximumTotal = null)
+    {
         $this->calculations[] = [
             'calculation' => $calculation,
             'input' => $input,
